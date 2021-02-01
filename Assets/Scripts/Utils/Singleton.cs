e8dde2627f48ca58b54c31d0433d62a94c331b3c
Bruno Tabosa <brunotabosasnt@gmail.com>
2021-02-01 15:15:48 -0300

    Improving Animations

--- Assets/Scripts/Utils/Singleton.cs
+++ Assets/Scripts/Utils/Singleton.cs
@@ -3,14 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 {
-    // Check to see if we're about to be destroyed.
     private static bool m_ShuttingDown = false;
     private static object m_Lock = new object();
     private static T m_Instance;
 
-    /// <summary>
-    /// Access singleton instance through this propriety.
-    /// </summary>
     public static T Instance
     {
         get
